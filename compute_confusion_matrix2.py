0ba6d34371eb48e99c0d798ff7105bf4f39d320f
Chaomin <lycanfirst@gmail.com>
2017-04-30 15:16:02 +1000

    add_save_method

--- compute_confusion_matrix2.py
+++ compute_confusion_matrix2.py
@@ -18,9 +18,9 @@ def compute_confusion_matrix(true_label_file, predicted_label_file,label_address
      return confusion_matrix_, label_list
  
 
-true_label_file_address = 'C:/Users/Chaomin/Desktop/data_mining/assignment1_2017S1//train_and_test_data/my_test_label.csv'
-predict_label_file_address = 'C:/Users/Chaomin/Desktop/data_mining/assignment1_2017S1/result/logit_test_label.csv'
-label_address = 'C:/Users/Chaomin/Desktop/data_mining/assignment1_2017S1/group_label.csv'
+true_label_file_address = 'C:/Users/Chaomin/Desktop/new/data_mining/data/train_test_data'
+predict_label_file_address = 'C:/Users/Chaomin/Desktop/new/data_mining/data/result/logit_test_label.csv'
+label_address = 'C:/Users/Chaomin/Desktop/new/data_mining/data/intermieidate/group_label.csv'
 
 #==============================================================================
 # matrix, label_list = compute_confusion_matrix(true_label_file_address, predict_label_file_address, label_address)
@@ -40,29 +40,26 @@ class confusion_matrix_builder:
         self.predicted_label_file_address = predicted_label_file_address
         self.label_address = label_address
     
-    def compute_confusion_matrix(self,save,*args):
-        if len(args) == 2:
-            matrix_without_header = args[0]
-            matrix_name = args[1]
+    def compute_confusion_matrix(self):
         true_label = np.genfromtxt(self.true_label_file_address, delimiter = ',',dtype=str)
         predicted_label = np.genfromtxt(self.predicted_label_file_address, delimiter = ',', dtype= str)
         label_list = list(np.genfromtxt(label_address, delimiter = ',',dtype= str))
         confusion_matrix_ = confusion_matrix(true_label, predicted_label, labels = label_list)
-        if save:
-            np.savetxt(matrix_without_header, confusion_matrix_,fmt='%5d', delimiter=',')
-            df_matrix = pd.DataFrame(confusion_matrix_)
-            df_matrix.columns = label_list
-            df_matrix.insert(0,'true_label||predicted_label', pd.Series(label_list))
-            df_matrix.to_csv(matrix_name, index = False)
- 
-            
+
         return confusion_matrix_, label_list
     
+
+
+    def save_file(self, confusion_matrix, label_list, matrix_withheader_address, matrix_address):
+        np.savetxt(matrix_address, confusion_matrix, fmt='%5d', delimiter=',')
+        df_matrix = pd.DataFrame(confusion_matrix)
+        df_matrix.columns = label_list
+        df_matrix.insert(0,'true_label||predicted_label', pd.Series(label_list))
+        df_matrix.to_csv(matrix_withheader_address, index = False)
 
 if __name__ == '__main__':
     k = confusion_matrix_builder(true_label_file_address,predict_label_file_address,label_address)
     #m,n = k.compute_confusion_matrix(False)
-    m,n = k.compute_confusion_matrix(True, 'old.csv','new.csv')
+    m,n = k.compute_confusion_matrix()
 
 
-# I just need this to test my code
