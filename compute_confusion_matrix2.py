e86464036f85ab265f6245115f21c8c67a7d638c
Chaomin Yuan <lycanfirst@gmail.com>
2017-05-01 12:47:01 +1000

    add_function_of_acc_and_so_on

--- compute_confusion_matrix2.py
+++ compute_confusion_matrix2.py
@@ -24,18 +24,58 @@ def r_c_sum(matrix, row, column):
 def calculate_r_c_sum(matrix_arg):
     matrix = matrix_arg
     new_row = np.zeros((1,len(matrix)))
-    new_column = np.empty([len(matrix) + 1, 1])
+    new_column = np.zeros((len(matrix) + 1, 1))
     matrix = np.concatenate([matrix,new_row], axis=0)
     matrix = np.concatenate([matrix,new_column], axis = 1)
-    print(np.shape(matrix))
-    print(np.shape(new_column))
-    print(np.shape(new_row))
-    print(matrix)
+    # print(np.shape(matrix))
+    # print(np.shape(new_column))
+    # print(np.shape(new_row))
+    # print(matrix)
     for row in range(0,len(matrix)):
         matrix[row, len(matrix) - 1] = r_c_sum(matrix, row,'all')
     for column in range(0,len(matrix)):
         matrix[len(matrix) - 1, column] = r_c_sum(matrix, 'all', column)
     return matrix
+
+def calculate_acc(matrix, row_header_l, column_header_l, label_l):
+    matrix_len = len(matrix)
+    result_matrix = np.zeros((matrix_len, len(row_header_l)))
+    row_header = np.matrix(row_header_l)
+    result_matrix = np.concatenate([row_header, result_matrix], axis=0)
+    l.insert(0, 'binary_classifier_by_label')
+    column_header = np.matrix(l).T
+    result_matrix = np.concatenate([column_header, result_matrix], axis=1)
+    result_matrix = np.delete(result_matrix, 1, 1)
+    result_df = pd.DataFrame(result_matrix, columns=row_header_l)
+    result_sub_df = result_df.iloc[1:, 1:len(row_header_l)]
+
+    for i in range(0,matrix_len):
+        result_sub_df.set_value(i + 1,'TP',j_2[i,i])
+        result_sub_df.set_value(i + 1, 'FN', j_2[matrix_len,i])
+        result_sub_df.set_value(i + 1, 'FP', j_2[i,matrix_len])
+        result_sub_df.set_value(i + 1, 'TN', np.sum(j_2)-(j_2[i,i] + j_2[matrix_len,i] + j_2[i,matrix_len]))
+
+    for i in range(0,matrix_len):
+        TP = result_sub_df.get_value(i+1,'TP')
+        TN = result_sub_df.get_value(i+1,'TN')
+        FN = result_sub_df.get_value(i+1,'FN')
+        FP = result_sub_df.get_value(i+1,'FP')
+
+        acc = (TP+TN)/(TP+TN+FN+FP)
+        spe = TN/(TN + FN)
+        pre = TP/(TP+FP)
+        f1 =  2*TP/(2*TP + FP + FN)
+
+        result_sub_df.set_value(i + 1, 'ACC', acc)
+        result_sub_df.set_value(i + 1, 'SPE', spe)
+        result_sub_df.set_value(i + 1, 'PRE', pre)
+        result_sub_df.set_value(i + 1, 'F1', f1)
+    del l[0]
+    result_sub_df.insert(0, 'binary_classifier_by_label', l)
+
+    return result_sub_df
+
+
 
 class confusion_matrix_builder:
     def __init__(self, true_label_file_address, predicted_label_file_address, label_address):
@@ -84,4 +124,44 @@ if __name__ == '__main__':
     # print(r_c_sum(a, 'all', 0))
     # print(r_c_sum(a, 'all', 1))
     j_2 = calculate_r_c_sum(j)
-    pd.DataFrame(j_2).to_csv('C:/Users/Chaomin/Desktop/data_mining/data/result/test_sum.csv')
+    #pd.DataFrame(j_2).to_csv('C:/Users/Chaomin/Desktop/data_mining/data/result/test_sum.csv')
+
+    # result_matrix = np.zeros((len(j), 9))
+    # row_header = np.matrix(['binary_classifier_by_label','TP','FN','FP','TN','ACC','SPE','PRE','F1'])
+    # result_matrix = np.concatenate([row_header, result_matrix], axis = 0)
+    # l.insert(0, 'binary_classifier_by_label')
+    # column_header = np.matrix(l).T
+    # result_matrix = np.concatenate([column_header,result_matrix], axis = 1)
+    # result_matrix = np.delete(result_matrix, 1,1)
+    # print('result_matrix',np.shape(result_matrix))
+    # print('row_header', np.shape(row_header))
+    # print('column_header',np.shape(column_header))
+    #
+    # #print(result_matrix)
+    # result_df = pd.DataFrame(result_matrix, columns=['binary_classifier_by_label','TP','FN','FP','TN','ACC','SPE','PRE','F1'])
+    # result_sub_df = result_df.iloc[1:,1:9]
+    # print(np.shape(result_sub_df))
+    # #result_sub_df
+    # for i in range(0,30):
+    #     result_sub_df.set_value(i + 1,'TP',j_2[i,i])
+    #     result_sub_df.set_value(i + 1, 'FN', j_2[30,i])
+    #     result_sub_df.set_value(i + 1, 'FP', j_2[i,30])
+    #     result_sub_df.set_value(i + 1, 'TN', np.sum(j_2)-(j_2[i,i] + j_2[30,i] + j_2[i,30]))
+    #
+    # for i in range(0,30):
+    #     TP = result_sub_df.get_value(i+1,'TP')
+    #     TN = result_sub_df.get_value(i+1,'TN')
+    #     FN = result_sub_df.get_value(i+1,'FN')
+    #     FP = result_sub_df.get_value(i+1,'FP')
+    #
+    #     acc = (TP+TN)/(TP+TN+FN+FP)
+    #     spe = TN/(TN + FN)
+    #     pre = TP/(TP+FP)
+    #     f1 =  2*TP/(2*TP + FP + FN)
+    #     result_sub_df.set_value(i + 1, 'ACC', acc)
+    #     result_sub_df.set_value(i + 1, 'SPE', spe)
+    #     result_sub_df.set_value(i + 1, 'PRE', pre)
+    #     result_sub_df.set_value(i + 1, 'F1', f1)
+    #
+    # del l[0]
+    # result_sub_df.insert(0, 'binary_classifier_by_label', l)
