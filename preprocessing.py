5ff93a3881401e6b2f3a0d1a858f173691f3c918
Chaomin Yuan <lycanfirst@gmail.com>
2017-05-05 13:01:55 +1000

    add add split_function (first version)

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn import preprocessing
+import pandas as pd
+import logistic_classifier as lc
 
 def scale(X, with_mean = True, with_std = True, axis=0):
     np.asarray(X)
@@ -18,8 +19,35 @@ def scale2(X, axis = 0):
     X = (X - column_min)/(column_max - column_min)
 
     return X
-a = np.matrix([[0,0,103],[40,50,6],[78,8,9]])
 
-b = preprocessing.scale(a)
-print(b)
+def split(X, test_data_size_ratio):
+    """
+     Split an data into two set, one is the index of training data set, and another is the  index oftest data set
+     
+    :param X: The data matrix with shape(m,n), where m is sample size and n is feature size 
+    :param test_size: specify the size of test data
+    :return: two array , one is the index of a training data, anther is  the index of test data 
+    """
+    m,n = np.shape(X)
+    index = np.zeros((m,1))
+    for i in range(m):
+        index[i] = i
+    X = np.concatenate([index,X], axis = 1)
+    index = index.flatten()
 
+    test_size = round(float(test_data_size_ratio) * len(index))
+    X_test = np.random.choice(len(index), test_size, replace=False)
+
+    X_df = pd.DataFrame(X)
+
+    return X, X_test,X_df
+
+if __name__ == "__main__":
+    #a = np.matrix([[0,0,103],[40,50,6],[78,8,9]])
+    data_address = 'C:/Users/Chaomin/Desktop/data_mining/data/classifier_data_for_test/test_data_for_logit_r/logit_train_data.csv'
+    label_address = 'C:/Users/Chaomin/Desktop/data_mining/data/classifier_data_for_test/test_data_for_logit_r/logit_train_labelCopy.csv'
+    predict_matrix_address = 'C:/Users/Chaomin/Desktop/data_mining/data/classifier_data_for_test/test_data_for_logit_r/logit_test_data.csv'
+    data_loader = lc.DataLoader(data_address, label_address, predict_matrix_address)
+    data, label, predict_matrix, unique_label = data_loader.return_value()
+
+    q,w,e = split(data, 0.5)
